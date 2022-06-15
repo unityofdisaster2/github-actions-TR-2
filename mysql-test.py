23774ac4b3586626183561c87cd21fafdd41ee17
Raymundo Rosales <rosalesreyesraymundo@gmail.com>
2022-06-15 11:37:41 -0500

    add python client

---
+++ mysql-test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python 
+import mariadb 
+
+conn = mariadb.connect(
+    user="miusuario0",
+    password="mipassword0",
+    host="mariadb_service",
+    database="demo")
+cur = conn.cursor() 
+
+some_name = "Georgi" 
+cur.execute("SELECT CategoryID,CategoryName, Description FROM Categories") 
+
+for CategoryID, CategoryName, Description in cur: 
+    print(f"CategoryID: {CategoryID}, Last name: {Description}")
+    
+# #insert information 
+# try: 
+#     cur.execute("INSERT INTO employees (first_name,last_name) VALUES (?, ?)", ("Maria","DB")) 
+# except mariadb.Error as e: 
+#     print(f"Error: {e}")
+
+# conn.commit() 
+# print(f"Last Inserted ID: {cur.lastrowid}")
+    
+conn.close()
